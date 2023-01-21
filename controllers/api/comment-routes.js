d85c8ad331e00970931dd9e2bd90247bf06442cb
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2023-01-21 09:55:15 -0500

    Added api routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { response } = require('express');
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// GET route
+router.get('/', withAuth, (req, res) => {
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST route
+router.post('/', withAuth, (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      user_id: req.session.user_id,
+      post_id: req.body.post_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+// DELETE route
+router.delete('/:id', withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found.' });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
